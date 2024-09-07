b836e752a254d12f137a0a06e5974ef0545bd366
Isaque Roberto <isaquezulato@gmail.com>
2024-09-07 00:30:32 -0300

    add

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ function pesquisar() {
             <a href="#" target="_self">${dado.titulo}</a> 
         </h2>
         <p class="descricao-meta">${dado.descricao}</p>
-        <a href=${dado.link} target="${dado.linked ? "_self" : "_blank"}">Saiba mais</a>
+        <a href=${dado.link} target=_blank"}">Saiba mais</a>
     </div>
 `
     }
