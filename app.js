d1ac579c1cd739a7c54c1d33d2a74e3f285505bc
Isaque Roberto <isaquezulato@gmail.com>
2024-09-06 22:57:03 -0300

    add finalized

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-console.log(dados);
+function pesquisar() {
+  // Obtém a seção onde os resultados da pesquisa serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
+
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+  // se o campoPesquisa for uma string vazia
+  if (campoPesquisa == "") {
+    section.innerHTML = "<p>Escreva uma palavra na barra</p>"
+    return
+  }
+
+  campoPesquisa = campoPesquisa.toLowerCase()
+
+  // Inicializa uma string vazia para armazenar os resultados da pesquisa
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+
+  // Itera sobre cada dado na lista de dados (assumindo que 'dados' é um array de objetos)
+  for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    tags = dado.tags.toLowerCase()
+    // se  titulo includes campoPesquisa
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+        // cria um novo elemento
+    resultados += `
+    <div class="item-resultado">
+        <h2>
+            <a href="#" target="_blank">${dado.titulo}</a> 
+        </h2>
+        <p class="descricao-meta">${dado.descricao}</p> 
+        <a href=${dado.link} target="_self">Saiba mais</a> 
+    </div>
+`;
+    }
+    
+  }
+
+  if (!resultados) {
+    resultados = "<p>Nada foi encontrdado</p>"
+  }
+
+  // Atribui o HTML gerado à seção de resultados
+  section.innerHTML = resultados;
+}
--- dados.js
+++ dados.js
@@ -5,20 +5,23 @@ let dados = [
     descricao:
       "O autismo, ou Transtorno do Espectro Autista (TEA), é uma condição complexa do neurodesenvolvimento que afeta a forma como uma pessoa se comunica e interage com o mundo ao seu redor.",
     link: "https://autismoerealidade.org.br/o-que-e-o-autismo/",
+    tags: "autismo pessoa tea comunica neuro"
   },
   // Novo objeto: Quem sou eu?
   {
     titulo: "Quem sou eu?",
     descricao:
       "Sou Isaque Roberto, um desenvolvedor iniciante apaixonado por tecnologia e música. Gosto de ler, desenhar, jogar e programar. Meu sonho é trabalhar como desenvolvendo jogos e fazer um mangá para que vire um anime.",
-    link: "", // Link não foi fornecido para essa seção
+    link: "./quemsoueu/index.html",
+    tags: "eu ler  livro jogar"
   },
   // Novo objeto: Meus hobbies
   {
     titulo: "Meus hobbies",
     descricao:
       "Em meus momentos de lazer, adoro ler um mangá ou livro, também leio Hqs, mas no momento não estou lendo, jogo muito no meu Ps4 e assistir filmes ou animes. Além disso, estou de aprendendo um novo idioma, o Inglês.",
-    link: "", // Link não foi fornecido para essa seção
+    link: "./hobby/index.html",
+    tags: "eu ler desenhar  jogar"
   },
   // Novo objeto: A Importância da Inclusão de Pessoas com Autismo no Mercado de Trabalho
   {
@@ -26,6 +29,7 @@ let dados = [
     descricao:
       "A inclusão de pessoas com autismo no mercado de trabalho é fundamental para construir uma sociedade mais justa e inclusiva. Pessoas com TEA possuem habilidades únicas, como alta capacidade de concentração, atenção aos detalhes e pensamento lógico, que podem ser grandes ativos para as empresas. No entanto, ainda enfrentam diversos desafios para ingressar e se manter no mercado de trabalho.",
     link: "https://www.autismoemdia.com.br/blog/autismo-no-mercado-de-trabalho-desafios-e-oportunidades/",
+    tags: "autismo pessoa tea comunica"
   },
   // Novo objeto: Desafios Enfrentados por Pessoas com Autismo no Mercado de Trabalho
   {
@@ -33,5 +37,6 @@ let dados = [
     descricao:
       "Pessoas com autismo enfrentam diversos obstáculos ao buscar um emprego. Entre eles estão: preconceito e falta de conhecimento sobre o TEA, dificuldade em se comunicar de forma convencional, desafios em ambientes sociais complexos e falta de adaptações no ambiente de trabalho. É fundamental que empresas e sociedade como um todo trabalhem para superar esses desafios e promover a inclusão.",
     link: "https://www.conjur.com.br/2024-abr-11/desafios-enfrentados-pelo-autista-e-seus-familiares-no-meio-ambiente-do-trabalho/",
+    tags: "autismo pessoa tea comunica"
   },
 ];
